db8660ffccd03f56e92eb1836c73f352554faccf
Ashitosh Kole <ashitoshkole007@gmail.com>
2025-09-03 16:49:03 +0530

    updated successfully on 3-09-2025

--- src/test/java/api/tests/AlbumsTests.java
+++ src/test/java/api/tests/AlbumsTests.java
@@ -1,10 +1,13 @@
 package api.tests;
 
+import java.util.Arrays;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import api.endpoints.AlbumsEndpoints;
 import io.restassured.response.Response;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class AlbumsTests extends BaseTest {
 	
@@ -13,11 +16,34 @@ public class AlbumsTests extends BaseTest {
 	public void getAlbums() {
 		
 		
-		
 		Response response = AlbumsEndpoints.getAlbums(generateToken());
 		         response.then().log().all();
 		         
+		        response.then()
+		        .body("album_type", equalTo("album"))
+	            .body("id", equalTo("4aawyAB9vmqN3uQ7FjRGTy"))
+	            .body("total_tracks", greaterThan(0))
+	            .body("available_markets", hasItems("AT", "BE", "BG"))
+	            .body("images", hasSize(greaterThan(0)))
+	            .body("images.url", everyItem(startsWith("https://")));
+		        
+		        System.out.println("upc : "+ response.body().jsonPath().getString("artists[0].external_urls"));
+		         		         
 		         Assert.assertEquals(response.statusCode(), 200);
+		         Assert.assertEquals(response.body().jsonPath().getString("album_type"), "album");
+		         Assert.assertEquals(response.body().jsonPath().getString("id"),"4aawyAB9vmqN3uQ7FjRGTy");
+		
+	}
+	
+	@Test(priority=1)
+	public void getAlbumsUnauthorized() {
+		
+		
+		
+		Response response = AlbumsEndpoints.getAlbums("BrDpKTttGqTet3uM_dE-fAfS9vo-rJoAGvGXRAOfCTsGUxZKO4-XzhNn9wEplANZfPRJPQRmTyR9Q36GHjvq8gqg20HmwStY3usE-T-0yfqC7fwf130UozrFC5DnDemSwlMH-VYYJTs");
+		         response.then().log().all();
+		         
+		         Assert.assertEquals(response.statusCode(), 401);
 		
 	}
 	
